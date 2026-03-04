0e9663150e663a89ed495b2e6903746f0e03bd16
katiedayJunk1 <katie.day@junkluggers.com>
2026-03-03 18:52:32 -0600

    adding constant pool

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('./database');
-
+const { Pool } = require('pg'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const connectionString = process.env.DATABASE_URL
